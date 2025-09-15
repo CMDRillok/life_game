bcb52aedb2b971de460abd59c74c4fdee91f9351
CMDRillok <romapridieb@gmail.com>
2025-09-15 19:35:54 +0300

    first

---
+++ main.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pygame as pg
+
+desk = np.zeros((10, 10), dtype=int)
+
+class Plant:
+    def __init__(self):
+        list_of_components = []
+
+class Component:
+    pass
+
+
+class Green(Component):
+    def __init__(self):
+        pass
+
+class Red(Component):
+    def __init__(self):
+        pass
+
+class Yellow(Component):
+    def __init__(self):
+        pass
+
+class White(Component):
+    def __init__(self):
+        pass
+
+class Blue(Component):
+    def __init__(self):
+        pass
