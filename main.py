92d79bce210bc97617d995bfb7e3da0daf6b21bc
CMDRillok <romapridieb@gmail.com>
2025-09-18 22:31:29 +0300

    сделано засеивание поля

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
 
+X, Y = (20, 20)
+
 class Zero:
-    def get():
+    def get(self):
         return 0
 
 class Desk:
     def __init__(self):
-        self.desk = np.full((10, 10), Zero)
+        self.desk = np.full((X, Y), Zero)
 
     def get(self):
         return self.desk
@@ -16,12 +18,29 @@ class Desk:
         for i in range(desk_array.shape[0]):
             row = []
             for j in range(desk_array.shape[1]):
-                row.append(desk_array[i, j].get())
+                try:
+                    row.append(desk_array[i, j].get())
+                except TypeError:
+                    row.append(0)
             print(row)
 
+    def create(self, x, y, obj):
+        self.desk[x, y] = obj
 
-desk = Desk()
-desk.print_desk()
+    def make_random_desk(self, count=10):
+        """Наполняет поле семенами с равномерным распределением"""
+        # Рассчитываем шаг для равномерного распределения
+        step_i = max(1, Y // int((count) ** 0.5))
+        step_j = max(1, X // int((count) ** 0.5))
+
+        placed = 0
+        for i in range(0, Y, step_i):
+            for j in range(0, X, step_j):
+                if placed < count:
+                    self.create(i, j, Blue())
+                    placed += 1
+                if placed >= count:
+                    return
 
 class Plant:
     def __init__(self):
@@ -71,3 +90,7 @@ class Blue(Component):
         super().__init__()
         self.num = 5
 
+
+desk = Desk()
+desk.make_random_desk(count=100)
+desk.print_desk()
